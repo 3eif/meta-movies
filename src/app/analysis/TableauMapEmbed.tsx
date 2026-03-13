b202218e65f808aeaa59d51525f00122b170d403
Seif <seifaziz10@gmail.com>
2026-03-13 10:44:03 -0700

    fix(website): update Tableau embed parameters and image sources

--- src/app/analysis/TableauMapEmbed.tsx
+++ src/app/analysis/TableauMapEmbed.tsx
@@ -3,12 +3,12 @@
 import { useEffect } from "react";
 
 const EMBED_HTML = `
-  <div class="tableauPlaceholder" id="viz1772819769351" style="position: relative">
+  <div class="tableauPlaceholder" id="viz1773423519324" style="position: relative">
     <noscript>
       <a href="#">
         <img
           alt="Global Film Production by Country"
-          src="https://public.tableau.com/static/images/GD/GDBFY787W/1_rss.png"
+          src="https://public.tableau.com/static/images/2X/2XG9CB7WQ/1_rss.png"
           style="border: none"
         />
       </a>
@@ -16,9 +16,9 @@ const EMBED_HTML = `
     <object class="tableauViz" style="display:none;">
       <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
       <param name="embed_code_version" value="3" />
-      <param name="path" value="shared/GDBFY787W" />
+      <param name="path" value="shared/2XG9CB7WQ" />
       <param name="toolbar" value="yes" />
-      <param name="static_image" value="https://public.tableau.com/static/images/GD/GDBFY787W/1.png" />
+      <param name="static_image" value="https://public.tableau.com/static/images/2X/2XG9CB7WQ/1.png" />
       <param name="animate_transition" value="yes" />
       <param name="display_static_image" value="yes" />
       <param name="display_spinner" value="yes" />
@@ -32,7 +32,7 @@ const EMBED_HTML = `
 
 export default function TableauMapEmbed() {
   useEffect(() => {
-    const divElement = document.getElementById("viz1772819769351");
+    const divElement = document.getElementById("viz1773423519324");
     if (!divElement) return;
 
     const vizElement = divElement.getElementsByTagName("object")[0];
